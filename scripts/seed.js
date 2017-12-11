bbc7f5bc5bb26ec4422c77459e02ca11a5dc6e2b
drnextgis <rykovd@gmail.com>
2017-12-11 19:35:43 +0700

    multiple layers example

--- scripts/seed.js
+++ scripts/seed.js
@@ -5,10 +5,12 @@ var geojson2mvt = require('geojson2mvt');
 var cities = JSON.parse(fs.readFileSync('source/cities.json'));
 var zones = JSON.parse(fs.readFileSync('source/zones.json'));
 cities.features.map(function(city) {
-  console.log(city.properties.name);
   var bbox = extent(city.geometry);
   var options = {
-    layerName: 'cities',
+    layers: {
+       'cities': cities,
+       'zones': zones
+    },
     rootDir: 'cache',
     bbox: [bbox[1], bbox[0],
            bbox[3], bbox[2]],
@@ -17,5 +19,5 @@ cities.features.map(function(city) {
       max: 15
     }
   };
-  geojson2mvt(zones, options);
+  geojson2mvt(options);
 });
